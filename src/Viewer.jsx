649ecff6f9560c670917c65f949deda6c6149196
Simeon Rolev <srolev@vectorworks.net>
2023-07-25 16:37:10 +0300

    Zoom to target

--- src/Viewer.jsx
+++ src/Viewer.jsx
@@ -1,6 +1,9 @@
 import { pdfjs } from 'react-pdf';
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
 
 import React from 'react'
 import PropTypes from 'prop-types'
@@ -23,54 +26,167 @@ function isElementInViewport(el) {
     rect.top < (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */;
 }
 
+function detectMouseWheelDirection(e) {
+  var delta = null,
+    direction = false;
+  if (!e) { // if the event is not provided, we get it from the window object
+    e = window.event;
+  }
+  if (e.wheelDelta) { // will work in most cases
+    delta = e.wheelDelta / 60;
+  } else if (e.detail) { // fallback for Firefox
+    delta = -e.detail / 2;
+  }
+  if (delta !== null) {
+    direction = delta > 0 ? 'up' : 'down';
+  }
+
+  return direction;
+}
+
+let whset = false
+
+
 function Viewer(props) {
   const [numPages, setNumPages] = useState();
+  /* This is a temp zoom level while zooming with the scroll wheel */
   const [scale, setScale] = useState(1);
+  /* This is the final scale used for the PDF viewer */
+  const [zoom, setZoom] = useState(1);
+
+  const [_width, setWidth] = useState();
+  const [_height, setHeight] = useState();
 
   const pageRefs = {}
-
-  const zoomIn = () => {
-    setScale(scale + 0.2)
-  }
-  const zoomOut = () => {
-    setScale(scale - 0.2)
-  }
+  let documentRef = null;
+  let rescaledRef = React.useRef();
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
 
+  /* 
+    https://github.com/wojtekmaj/react-pdf/issues/493
+  */
   React.useEffect(() => {
-    document.addEventListener('scroll', () => {
-      Object
-        .keys(pageRefs)
-        .forEach(pn => {
-          if (pn) {
-            console.log([pn, isElementInViewport(pageRefs[pn])])
-          }
-        })
-    
-    })
-  })
+    const area = document.getElementById("available-space")
+    // let timeout = null;
+
+    const onWheel = (e) => {
+      if (e.ctrlKey) {
+
+        // if (timeout) clearTimeout(timeout);
+        // timeout = setTimeout(() => {
+        //   clearTimeout(timeout);
+        //   setZoom(zoom * scale);
+        //   setScale(1);
+        //   // zoom to proper point here
+        // }, 1000)
+
+        e.preventDefault()
+        const { scrollHeight, scrollWidth } = area;
+
+        const xPx = e.clientX + area.scrollLeft - area.offsetLeft
+        const yPx = e.clientY + area.scrollTop - area.offsetTop
+
+        const xPerc = xPx / scrollWidth
+        const yPerc = yPx / scrollHeight
+
+        // Those don't need to be in a state. directly add them to ref.current.styles.transform = scale(...)
+        const currentScale = rescaledRef.current.style.transform.slice(6, rescaledRef.current.style.transform.length - 1)
+        console.log("scaling to:", parseFloat(currentScale) + (detectMouseWheelDirection(e) === 'up' ? 0.1 : -0.1))
+        rescaledRef.current.style.transform = `scale(${parseFloat(currentScale) + (detectMouseWheelDirection(e) === 'up' ? 0.1 : -0.1)})`
+
+          const { scrollHeight: scrollHeight2, scrollWidth: scrollWidth2 } = area;
+  
+          const widthDiff = scrollWidth2 - scrollWidth
+          const heightDiff = scrollHeight2 - scrollHeight
+  
+          console.log(widthDiff, heightDiff)
+  
+          area.scrollTo(
+            area.scrollLeft + widthDiff * xPerc,
+            area.scrollTop + heightDiff * yPerc
+          )
+      }
+    }
+
+    document.body.addEventListener('wheel', onWheel, { passive: false });
+    return () => document.body.removeEventListener('wheel', onWheel, { passive: false })
+  }, [scale, zoom])
+
+  // React.useEffect(() => {
+  //   documentRef.style.transform = `scale(1)`
+  //   Object
+  //       .values(pageRefs)
+  //       .forEach(page => {
+  //         page.style.transform = `scale(${scale})`
+  //       })
+  // }, [scale, pageRefs, documentRef])
+
+
+  const onDocumentRef = ref => {
+    if (ref) {
+      documentRef = ref;
+
+      if (!whset) {
+        whset = true;
+        setTimeout(() => {
+          const { width, height } = ref.getBoundingClientRect();
+          setWidth(width);
+          setHeight(height);
+        }, 500)
+
+      }
+    }
+  }
 
   return (
-    <div>
-      <div style={{ position: "fixed", top: 0, zIndex: 10 }}>
-        <button onClick={zoomIn}>+</button>
-        <button onClick={zoomOut}>-</button>
-      </div>
+    <div
+      id="available-space"
+      style={{
+        maxWidth: '100%',
+        maxHeight: '100%',
+        display: 'flex',
+        overflow: 'scroll',
+      }}
+    >
 
-      <Document file="Public Library Sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        {Array(numPages).fill(42).map((_, index) => {
-          return (
-            <Page key={index}
-              inputRef={ref => { pageRefs[index + 1] = ref; }}
-              scale={scale}
-              pageNumber={index + 1}
-            />
-          )
-        })}
-      </Document>
+      <div
+        id='canvas'
+        style={{
+          width: _width * scale,
+          height: _height * scale,
+          margin: 'auto'
+        }}
+      >
+        <div
+          id='rescaled'
+          ref={rescaledRef}
+          style={{
+            width: _width,
+            height: _height,
+            transform: `scale(${scale})`,
+            transformOrigin: '0 0',
+          }}
+        >
+          <Document
+            inputRef={onDocumentRef}
+            file="Public Library Sample.pdf"
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            {Array(numPages).fill(42).map((_, index) => {
+              return (
+                <Page key={index}
+                  inputRef={ref => { pageRefs[index + 1] = ref; }}
+                  // scale={zoom}
+                  pageNumber={index + 1}
+                />
+              )
+            })}
+          </Document>
+        </div>
+      </div>
     </div>
   )
 }
