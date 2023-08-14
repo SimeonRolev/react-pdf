b51fcc515c46b2af32af891bb5022218262280a1
Simeon Rolev <srolev@vectorworks.net>
2023-08-14 11:10:26 +0300

    Toggle point opacity on mouse over

--- src/Annotations/Point.jsx
+++ src/Annotations/Point.jsx
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types'
 import { Point } from '../point'
 
 function PointUI({point, ...rest}) {
+    const ref = React.useRef(null)
+
     return (
         <div
             className='vcs-pdf-point'
+            ref={ref}
             style={{
                 position: 'absolute',
                 top: point.top * 100 + '%',
@@ -13,8 +16,11 @@ function PointUI({point, ...rest}) {
                 border: '1px solid red',
                 transform: 'translate(-5px, -5px)',
                 width: 10,
-                height: 10
+                height: 10,
+                opacity: 0
             }}
+            onMouseOver={() => ref.current.style.opacity = 1}
+            onMouseLeave={() => ref.current.style.opacity = 0}
             {...rest}
         />
     )
